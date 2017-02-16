bac20d0cddb1ea7f11fb6f6288f4432bd55d2a12
abidmmp <abidmmp99@gmail.com>
2017-02-16 09:00:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -2461,3 +2461,15 @@ const retry = async (fn, attempts = 3, delay = 500) => {
     }
   }
 };
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
