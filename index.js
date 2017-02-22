ee9e207753b0b51aeee3f7ef3d89dc4c6feb84e6
abidmmp <abidmmp99@gmail.com>
2017-02-22 09:00:00 +0500

    Implement fetchJSON functionality

--- index.js
+++ index.js
@@ -2489,3 +2489,13 @@ const retry = async (fn, attempts = 3, delay = 500) => {
     }
   }
 };
+
+
+async function fetchJSON(url, options = {}) {
+  const res = await fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    ...options,
+  });
+  if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
+  return res.json();
+}
