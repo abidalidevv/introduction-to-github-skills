d60751cafc0b7ab92f1b9f6ea1e3c483edebdb0d
abidmmp <abidmmp99@gmail.com>
2017-03-03 09:00:00 +0500

    Optimize range performance

--- index.js
+++ index.js
@@ -2499,3 +2499,10 @@ async function fetchJSON(url, options = {}) {
   if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
   return res.json();
 }
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
