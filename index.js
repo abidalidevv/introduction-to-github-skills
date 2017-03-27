349ac4bfa2526a0d49144d9e9c1b4918bef67810
abidmmp <abidmmp99@gmail.com>
2017-03-27 11:34:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -2559,3 +2559,15 @@ const formatDate = (date, locale = 'en-US', options = {}) =>
     year: 'numeric', month: 'short', day: 'numeric',
     ...options,
   }).format(new Date(date));
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
