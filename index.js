7aeb60734556a1365b6280b8fcaf107e2371fa8b
abidmmp <abidmmp99@gmail.com>
2017-03-28 10:17:00 +0500

    Implement fetchJSON functionality

--- index.js
+++ index.js
@@ -2574,3 +2574,13 @@ const retry = async (fn, attempts = 3, delay = 500) => {
 
 
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+
+async function fetchJSON(url, options = {}) {
+  const res = await fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    ...options,
+  });
+  if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
+  return res.json();
+}
