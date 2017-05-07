7b1297bb8c03c57d512370ec3219dfb27918473a
abidmmp <abidmmp99@gmail.com>
2017-05-07 10:17:00 +0500

    Update flatten helper method

--- index.js
+++ index.js
@@ -2693,3 +2693,6 @@ async function fetchJSON(url, options = {}) {
   if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
   return res.json();
 }
+
+
+const flatten = (arr, depth = 1) => arr.flat(depth);
