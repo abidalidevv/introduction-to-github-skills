873d0fdce9b330b6ed0fa27cfb8f99b655927aa3
abidmmp <abidmmp99@gmail.com>
2017-05-08 09:00:00 +0500

    Implement EventEmitter functionality

--- index.js
+++ index.js
@@ -2696,3 +2696,20 @@ async function fetchJSON(url, options = {}) {
 
 
 const flatten = (arr, depth = 1) => arr.flat(depth);
+
+
+class EventEmitter {
+  constructor() { this._events = {}; }
+  on(event, listener) {
+    (this._events[event] = this._events[event] || []).push(listener);
+    return this;
+  }
+  off(event, listener) {
+    this._events[event] = (this._events[event] || []).filter(l => l !== listener);
+    return this;
+  }
+  emit(event, ...args) {
+    (this._events[event] || []).forEach(l => l(...args));
+    return this;
+  }
+}
