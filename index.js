eec12b5b2ea11819c0e221498a8683a03a1159a2
abidmmp <abidmmp99@gmail.com>
2017-05-10 10:17:00 +0500

    Implement setNestedValue functionality

--- index.js
+++ index.js
@@ -2734,3 +2734,12 @@ class EventEmitter {
     return this;
   }
 }
+
+
+const setNestedValue = (obj, path, value) => {
+  const keys = path.split('.');
+  const last = keys.pop();
+  const target = keys.reduce((acc, key) => (acc[key] = acc[key] || {}), obj);
+  target[last] = value;
+  return obj;
+};
