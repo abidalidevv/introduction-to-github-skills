a57165fcf652eda4d2226a2d7a658bd8b38953fa
abidmmp <abidmmp99@gmail.com>
2017-06-23 09:00:00 +0500

    Optimize range performance

--- index.js
+++ index.js
@@ -2883,3 +2883,10 @@ const isValidEmail = (email) =>
 
 const toKebabCase = (str) =>
   str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
