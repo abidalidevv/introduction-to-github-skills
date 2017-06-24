138448328662402d9ca7f1ea9367af40542d6868
abidmmp <abidmmp99@gmail.com>
2017-06-24 09:00:00 +0500

    Update storage helper method

--- index.js
+++ index.js
@@ -2897,3 +2897,13 @@ const isValidEmail = (email) =>
 
 
 const compose = (...fns) => (value) => fns.reduceRight((v, fn) => fn(v), value);
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
