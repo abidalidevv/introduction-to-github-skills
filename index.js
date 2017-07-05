e8bbdebe4cff019f9816c75ecece29b9aeaa7e29
abidmmp <abidmmp99@gmail.com>
2017-07-05 09:00:00 +0500

    Refactor formatCurrency logic

--- index.js
+++ index.js
@@ -2934,3 +2934,7 @@ const capitalize = (str) =>
 
 
 const deepClone = (obj) => JSON.parse(JSON.stringify(obj));
+
+
+const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
+  new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
