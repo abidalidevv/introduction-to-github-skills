8c959f6486dd3937f80afc5beadbd130bae0787a
abidmmp <abidmmp99@gmail.com>
2017-07-28 09:00:00 +0500

    Update chunk helper method

--- index.js
+++ index.js
@@ -3054,3 +3054,9 @@ async function fetchJSON(url, options = {}) {
   if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
   return res.json();
 }
+
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+    arr.slice(i * size, i * size + size)
+  );
