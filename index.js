13f0435c643195b62c2721917bf50e684ba19908
abidmmp <abidmmp99@gmail.com>
2017-08-30 09:00:00 +0500

    Optimize range performance

--- index.js
+++ index.js
@@ -3150,3 +3150,10 @@ const capitalize = (str) =>
 
 const isValidEmail = (email) =>
   /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
