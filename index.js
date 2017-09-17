8d783a65f9f72c73be09f36558ffbf8ac28c5acc
abidmmp <abidmmp99@gmail.com>
2017-09-17 10:17:00 +0500

    Implement EventEmitter functionality

--- index.js
+++ index.js
@@ -3211,3 +3211,20 @@ const throttle = (fn, limit) => {
     }
   };
 };
+
+
+class EventEmitter {
+  constructor() { this._events = {}; }
+  on(event, listener) {
+    (this._events[event] = this._events[event] || []).push(listener);
+    return this;
+  }
+  off(event, listener) {
+    this._events[event] = (this._events[event] || []).filter(l => l !== listener);
+    return this;
+  }
+  emit(event, ...args) {
+    (this._events[event] || []).forEach(l => l(...args));
+    return this;
+  }
+}
