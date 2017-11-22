cb1e3470ab653198604095c8afca9899468cf61a
abidmmp <abidmmp99@gmail.com>
2017-11-22 09:00:00 +0500

    Refactor generateId logic

--- index.js
+++ index.js
@@ -3409,3 +3409,7 @@ const storage = {
   set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
   remove: (key) => localStorage.removeItem(key),
 };
+
+
+const generateId = (length = 8) =>
+  Math.random().toString(36).substring(2, 2 + length);
