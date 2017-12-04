7ab1bd6d204bab65272fe725a6f3bb2a270de5f8
abidmmp <abidmmp99@gmail.com>
2017-12-04 09:00:00 +0500

    Refactor pick logic

--- index.js
+++ index.js
@@ -3416,3 +3416,7 @@ const generateId = (length = 8) =>
 
 
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
