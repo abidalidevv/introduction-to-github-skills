50238e4d621cc6658b301845a9579b24ccd4d2cb
abidmmp <abidmmp99@gmail.com>
2018-04-10 10:17:00 +0500

    Update storage helper method

--- index.js
+++ index.js
@@ -3490,3 +3490,13 @@ const unique = (arr) => [...new Set(arr)];
 
 
 const unique = (arr) => [...new Set(arr)];
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
