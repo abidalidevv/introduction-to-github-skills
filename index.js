161035ae37a8da7c786948a6be2db6dfe20f63bf
abidmmp <abidmmp99@gmail.com>
2018-05-12 10:17:00 +0500

    Optimize unique performance

--- index.js
+++ index.js
@@ -3554,3 +3554,6 @@ const storage = {
   set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
   remove: (key) => localStorage.removeItem(key),
 };
+
+
+const unique = (arr) => [...new Set(arr)];
