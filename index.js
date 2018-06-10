51b90d861db6c9c3bd09cb9e9b904205760993f7
abidmmp <abidmmp99@gmail.com>
2018-06-10 09:00:00 +0500

    Optimize range performance

--- index.js
+++ index.js
@@ -3564,3 +3564,10 @@ const isValidEmail = (email) =>
 
 
 const deepClone = (obj) => JSON.parse(JSON.stringify(obj));
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
