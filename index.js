9a9070c9a58c7e4c703db5ff21921969cd7f90fa
abidmmp <abidmmp99@gmail.com>
2018-06-17 09:00:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -3579,3 +3579,15 @@ const generateId = (length = 8) =>
 
 const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
   new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
