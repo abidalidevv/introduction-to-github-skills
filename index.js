80d3266afce5ce3c89ea5b350143dd3dd2e45952
abidmmp <abidmmp99@gmail.com>
2018-07-02 11:34:00 +0500

    Refactor pick logic

--- index.js
+++ index.js
@@ -3624,3 +3624,7 @@ const throttle = (fn, limit) => {
 
 const scrollToTop = (smooth = true) =>
   window.scrollTo({ top: 0, behavior: smooth ? 'smooth' : 'auto' });
+
+
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
