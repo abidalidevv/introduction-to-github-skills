e439905f731c0186a0b1c7b9395c57c46bd9d41a
abidmmp <abidmmp99@gmail.com>
2018-08-12 10:17:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -3691,3 +3691,15 @@ const isValidEmail = (email) =>
 
 const toKebabCase = (str) =>
   str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
