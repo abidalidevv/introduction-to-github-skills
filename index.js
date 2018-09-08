22532405a8546e29a66024b77ad4214c6d2b1c30
abidmmp <abidmmp99@gmail.com>
2018-09-08 09:00:00 +0500

    Update storage helper method

--- index.js
+++ index.js
@@ -3753,3 +3753,13 @@ const memoize = (fn) => {
     return result;
   };
 };
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
