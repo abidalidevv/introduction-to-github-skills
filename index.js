8fea72fd5943e41e04c5692479b682e8b4db31de
abidmmp <abidmmp99@gmail.com>
2018-10-05 09:00:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -3785,3 +3785,15 @@ const memoize = (fn) => {
     return result;
   };
 };
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
