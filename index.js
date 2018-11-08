fe20bbb36f328b3c12a67b4e63c60ddf9351e70e
abidmmp <abidmmp99@gmail.com>
2018-11-08 09:00:00 +0500

    Optimize memoize performance

--- index.js
+++ index.js
@@ -3797,3 +3797,15 @@ const retry = async (fn, attempts = 3, delay = 500) => {
     }
   }
 };
+
+
+const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
