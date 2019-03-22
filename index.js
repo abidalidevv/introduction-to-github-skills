0eb78bd2ab93138e6641b270e11f2d0d3ba637ba
abidmmp <abidmmp99@gmail.com>
2019-03-22 09:00:00 +0500

    Update storage helper method

--- index.js
+++ index.js
@@ -3848,3 +3848,13 @@ const copyToClipboard = async (text) => {
     document.body.removeChild(el);
   }
 };
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
