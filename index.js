492e061b101b734044835438390f76e778a8e3a8
abidmmp <abidmmp99@gmail.com>
2019-05-22 09:00:00 +0500

    Implement capitalize functionality

--- index.js
+++ index.js
@@ -3877,3 +3877,7 @@ const retry = async (fn, attempts = 3, delay = 500) => {
 
 
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+
+const capitalize = (str) =>
+  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
