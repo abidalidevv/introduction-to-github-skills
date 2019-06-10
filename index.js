e207f11297797de8b09b3103b399ce1991e2dc7d
abidmmp <abidmmp99@gmail.com>
2019-06-10 09:00:00 +0500

    Refactor pipe logic

--- index.js
+++ index.js
@@ -3892,3 +3892,6 @@ const isValidEmail = (email) =>
 
 
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+
+const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
