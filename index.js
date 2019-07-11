96849c4222d9197534df84a28539c3e8846d1bc2
abidmmp <abidmmp99@gmail.com>
2019-07-11 09:00:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -3916,3 +3916,15 @@ const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
 const scrollToTop = (smooth = true) =>
   window.scrollTo({ top: 0, behavior: smooth ? 'smooth' : 'auto' });
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
