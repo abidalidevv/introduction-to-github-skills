ddf58f64fb7e2cb75fc99bfcf103b4a92c4adec9
abidmmp <abidmmp99@gmail.com>
2019-09-25 09:00:00 +0500

    Refactor pipe logic

--- index.js
+++ index.js
@@ -3957,3 +3957,6 @@ const chunk = (arr, size) =>
   Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
     arr.slice(i * size, i * size + size)
   );
+
+
+const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
