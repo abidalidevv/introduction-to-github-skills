5d74bb6132d0004bd5416b08dee8afad7dac6a97
abidmmp <abidmmp99@gmail.com>
2020-05-08 10:17:00 +0500

    Refactor pick logic

--- index.js
+++ index.js
@@ -4024,3 +4024,7 @@ const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
 
 
 const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
+
+
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
