fb5139451e0d30abde3c8ac66b7a73faa37dac7b
abidmmp <abidmmp99@gmail.com>
2020-09-03 10:17:00 +0500

    Refactor formatCurrency logic

--- index.js
+++ index.js
@@ -4072,3 +4072,7 @@ const parseQueryString = (search = window.location.search) =>
 
 const queryParams = (params) =>
   '?' + new URLSearchParams(params).toString();
+
+
+const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
+  new Intl.NumberFormat(locale, { style: 'currency', currency }).format(amount);
