0772fc6d7754855723be319b8001b5ee6814c4a0
abidmmp <abidmmp99@gmail.com>
2021-05-05 11:34:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -4139,3 +4139,15 @@ const unique = (arr) => [...new Set(arr)];
 
 
 const flatten = (arr, depth = 1) => arr.flat(depth);
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
