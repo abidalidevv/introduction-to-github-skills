50574c9f92e701109ede6eedeea47f48667b06d3
abidmmp <abidmmp99@gmail.com>
2022-03-13 11:34:00 +0500

    Optimize memoize performance

--- index.js
+++ index.js
@@ -4259,3 +4259,15 @@ class EventEmitter {
     return this;
   }
 }
+
+
+const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
