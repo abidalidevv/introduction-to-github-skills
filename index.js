71c6f4f0eb7d28415ffa5179cfef77d9357d9408
abidmmp <abidmmp99@gmail.com>
2022-04-16 11:34:00 +0500

    Implement fetchJSON functionality

--- index.js
+++ index.js
@@ -4278,3 +4278,13 @@ const isValidEmail = (email) =>
 
 
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+
+async function fetchJSON(url, options = {}) {
+  const res = await fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    ...options,
+  });
+  if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
+  return res.json();
+}
