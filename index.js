6ddbfa4fddcfdf335eea89250747c85522deaf88
abidmmp <abidmmp99@gmail.com>
2022-08-02 09:00:00 +0500

    Update groupBy helper method

--- index.js
+++ index.js
@@ -4323,3 +4323,11 @@ const capitalize = (str) =>
 
 const toKebabCase = (str) =>
   str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+
+const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const k = typeof key === 'function' ? key(item) : item[key];
+    (acc[k] = acc[k] || []).push(item);
+    return acc;
+  }, {});
