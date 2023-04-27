c39d4e0f75ba4214aae063ef22da49f6da83e4da
abidmmp <abidmmp99@gmail.com>
2023-04-27 09:00:00 +0500

    Optimize memoize performance

--- index.js
+++ index.js
@@ -4455,3 +4455,15 @@ const copyToClipboard = async (text) => {
     document.body.removeChild(el);
   }
 };
+
+
+const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
