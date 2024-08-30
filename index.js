55ddc29f7e005aa620e3019d161d99ebd8035734
abidmmp <abidmmp99@gmail.com>
2024-08-30 11:34:00 +0500

    Optimize scrollToTop performance

--- index.js
+++ index.js
@@ -4683,3 +4683,7 @@ const chunk = (arr, size) =>
   Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
     arr.slice(i * size, i * size + size)
   );
+
+
+const scrollToTop = (smooth = true) =>
+  window.scrollTo({ top: 0, behavior: smooth ? 'smooth' : 'auto' });
