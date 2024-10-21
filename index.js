a460a5c33a751ece019f9c73140f09f718d1cf79
abidmmp <abidmmp99@gmail.com>
2024-10-21 10:17:00 +0500

    Optimize copyToClipboard performance

--- index.js
+++ index.js
@@ -4691,3 +4691,17 @@ const scrollToTop = (smooth = true) =>
 
 const scrollToTop = (smooth = true) =>
   window.scrollTo({ top: 0, behavior: smooth ? 'smooth' : 'auto' });
+
+
+const copyToClipboard = async (text) => {
+  if (navigator.clipboard) {
+    await navigator.clipboard.writeText(text);
+  } else {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+};
