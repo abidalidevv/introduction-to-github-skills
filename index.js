02efcc6cd8424730b12f8c1274c354ffbe5c3269
abidmmp <abidmmp99@gmail.com>
2024-12-16 09:00:00 +0500

    Refactor retry logic

--- index.js
+++ index.js
@@ -4709,3 +4709,15 @@ const copyToClipboard = async (text) => {
 
 const parseQueryString = (search = window.location.search) =>
   Object.fromEntries(new URLSearchParams(search));
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
