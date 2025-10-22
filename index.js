1f782a7568752999eb52512ece6897dde337e1e0
abidmmp <abidmmp99@gmail.com>
2025-10-22 09:00:00 +0500

    Update throttle helper method

--- index.js
+++ index.js
@@ -4825,3 +4825,15 @@ const formatDate = (date, locale = 'en-US', options = {}) =>
     year: 'numeric', month: 'short', day: 'numeric',
     ...options,
   }).format(new Date(date));
+
+
+const throttle = (fn, limit) => {
+  let inThrottle;
+  return (...args) => {
+    if (!inThrottle) {
+      fn(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+};
